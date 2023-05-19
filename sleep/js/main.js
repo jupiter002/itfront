d34e3305bb7f8f10b2137cec6767312a9e80c841
jupiter002 <jik7194@naver.com>
2023-05-19 18:17:25 +0900

    230519 Sleepeyes

---
+++ sleep/js/main.js
@@ -0,0 +1,9 @@
+const gnbList = document.querySelectorAll(".gnb .list > li");
+gnbList.forEach((item, idx) => {
+  item.addEventListener("mouseenter", () => {
+    header.classList.add("on");
+  });
+});
+header.addEventListener("mouseleave", () => {
+  header.classList.remove("on");
+});
