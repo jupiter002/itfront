a478390fb440d24cdaad1e82f7deb55ce060b056
jupiter002 <jik7194@naver.com>
2023-05-23 17:18:26 +0900

    230523 megaSlides

--- megazone/js/main.js
+++ megazone/js/main.js
@@ -1,3 +1,47 @@
+Splitting();
+
 const header = document.querySelector(".header");
-header.classList.add("on");
+header.classList.add("on"); // html과 js가 잘 연결되어 있는지 확인하려면
+//브라우저에서 개발자도구의 network을 볼것
 console.log(header);
+//사용자가 scroll을 해서 스크롤의 높이가 0보다 커지면 on을 켠다
+
+window.addEventListener("scroll", () => {
+  // event매개변수
+  const Y = window.scrollY;
+  console.log(Y);
+
+  if (Y > 0) {
+    header.classList.add("on");
+  } else {
+    header.classList.remove("on");
+  }
+});
+
+gsap.from(".main-visual .content .txt .char", { y: 100, opcaity: 0, ease: "power3", duration: 1, delay: 1 });
+
+new Swiper(".service .mask", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  loop: true,
+  loopAdditionalSlides: 1,
+  navigation: {
+    prevEl: ".service .mask .btn-prev",
+    nextEl: ".service .mask .btn-next",
+  },
+  pagination: {
+    el: ".service .mask .pagination",
+    clickable: true,
+  },
+});
+new Swiper(".partner .brand", {
+  slidesPerView: "auto",
+  speed: 1000,
+  loop: true,
+  loopedSlides: 5,
+
+  autoplay: {
+    delay: 10,
+    disableOnInteraction: false,
+  },
+});
